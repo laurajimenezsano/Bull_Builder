6647019e2a9348c31c0b7dba6e66a7b7776ff212
Laura Jimenez <laurajimenez95@gmail.com>
2021-05-15 14:21:30 -0400

    new docs

---
+++ config_Laura.py
@@ -0,0 +1,5 @@
+gkey = "AIzaSyBCE1Gfjb9jlApKC3jzng4AHVE2E-p14qk"
+gkey2 = "AIzaSyAtI-eJmP3UwvQqdg-aWk1I1dN755k5t7I"
+
+api_key = "6e9a076de8bb52785f3e7886d93b0ecc"
+walk_key = "00163e65ea6b8c6c999d3fd212b40f32"
